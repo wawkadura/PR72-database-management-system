efebbf3ea4f2ad91c2b842bbf3fad0216da0e99d
wawkadura <elwalid.kadura@gmail.com>
2022-02-05 12:46:09 +0000

    commit test

---
+++ documentation/test.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ifstream fichier("/com.docker.devenvironments.code/projet/fichier.txt");
+
+    if (fichier)
+    {
+        //L'ouverture s'est bien passée, on peut donc lire
+
+        string ligne; //Une variable pour stocker les lignes lues
+        fichier.seekg(15, ios::beg);
+        fichier >> ligne;
+        cout << ligne;
+        // while (getline(fichier, ligne)) //Tant qu'on n'est pas à la fin, on lit
+        // {
+        //     cout << ligne << endl;
+
+        //     //Et on l'affiche dans la console
+        //     //Ou alors on fait quelque chose avec cette ligne
+        //     //À vous de voir
+        // }
+    }
+    else
+    {
+        cout << "ERREUR: Impossible d'ouvrir le fichier en lecture." << endl;
+    }
+
+    return 0;
+}

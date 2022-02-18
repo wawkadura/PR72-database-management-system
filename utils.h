371df22a269c1218544c2b713ceb2d6b630c75d8
wilfrid beaunes <kesnelbeaunes@gmail.com>
2022-02-18 18:29:30 +0100

    utils

--- utils.h
+++ utils.h
@@ -63,41 +63,6 @@ struct insert_records {
     std::vector<insert_query> fields;
 };
 
-typedef struct {
-    char table_name[TEXT_LENGTH];
-    table_definition_t table_definition;
-} create_query_t;
-
-typedef struct {
-    char table_name[TEXT_LENGTH];
-    filter_t where_clause;
-} delete_query_t;
-
-typedef struct {
-    char table_name[TEXT_LENGTH];
-    table_record_t fields_names;
-    table_record_t fields_values;
-} insert_query_t;
-
-typedef struct {
-    char table_name[TEXT_LENGTH];
-    table_record_t set_clause;
-    filter_t where_clause;
-} update_or_select_query_t;
-
-struct query_result {
-    query_type_t query_type;
-    union {
-        char table_name[TEXT_LENGTH];
-        char database_name[TEXT_LENGTH];
-        create_query_t create_query;
-        delete_query_t delete_query;
-        insert_query_t insert_query;
-        update_or_select_query_t update_query;
-        update_or_select_query_t select_query;
-    } query_content;
-};
-
 struct index_entry {
     int field_type_t ;
     std::vector<field_definition> field_type ;
